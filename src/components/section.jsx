e75d5bcb1f534a9ce69a975a5497a962b20623bd
GoodnessAbass19 <118232578+GoodnessAbass19@users.noreply.github.com>
2023-01-27 09:05:56 +0100

    Update section.jsx

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -2,15 +2,15 @@ import image from './images/image-web-3-desktop.jpg'
 import React from 'react';
 const Section = () => {
     return (
-          <section className='w-full h-full md:py-10 mt-16 text-white  container mx-auto'>
+        <section className='w-full h-full md:py-10 mt-16 text-white  container mx-auto'>
             <div className=' flex-wrap md:flex'>
                 <div className=' md:max-w-screen-lg w-full mx-auto  md:flex flex-col '>
                     <div className='md:max-w-screen-lg md:w-full w-4/5 md:h-full h-full  mx-auto'>
                         <img src={image} alt="" className='md:h-full h-72' />
                     </div>
-                    <div className='flex md:flex-row flex-col md:mt-6 '>
+                    <div className='flex md:flex-col lg:flex-row md:mt-6 flex-wrap'>
                         <div className='md:mt-6 mt-6 mx-4'>
-                            <h1 className='font-bold text-6xl text-black capitalize px-2'>the bright future of web 3.0 ?</h1>
+                            <h1 className='font-bold text-5xl text-black capitalize px-2'>the bright future of web 3.0 ?</h1>
                         </div>
                         <div className='md:mt-6 mt-6 mx-4'>
                             <p className='md:text-lg text-gray-500 capitalize text-sm px-4 max-w-screen'>we dive into the evolution of web that claims to put the power of the platforms back into the hands of the power.but is it really fulfilling it promise?</p>
@@ -20,7 +20,7 @@ const Section = () => {
                         </div>
                     </div>
                 </div>
-                <div className='text-white max-w-screen-lg bg-black px-7 md:w-1/4  w-4/5 md:mt-0 mt-20 mx-auto'>
+                <div className='text-white max-w-screen-lg bg-black px-7 md:w-fit lg:w-4/5 md:mt-6 lg:mt-6 mt-20 mx-auto'>
                     <h2 className='text-5xl capitalize text-yellow-500 font-bold'>new</h2>
                     <div className='text-white grid grid-cols-1 divide-y pt-6'>
 
